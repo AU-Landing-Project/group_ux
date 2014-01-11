03f63c799c1e48cdebb21f54f6d4911abdf18ea5
Jon Dron <github@jondron.org>
2014-01-10 17:25:32 -0800

    bug fix and manifest update

--- start.php
+++ start.php
@@ -36,9 +36,9 @@ function group_ux_init() {
   add_group_tool_option('group_ux_lock_blog',elgg_echo('group_ux:lockblog'),false);
   
   // remove the blog add button if group admin has locked the blog
-  if(group_ux_lock_blog_enabled && elgg_is_active_plugin("blog")){
+  if(elgg_is_active_plugin("blog")){
 	$group = elgg_get_page_owner_entity();			
-	if($group instanceof ElggGroup){
+	if($group instanceof ElggGroup && $group->group_ux_lock_blog_enable == 'yes'){
 		//unregister the add button for non admins
 		if(!$group->canEdit() && elgg_get_context()=='blog'){	  
 		//	elgg_unregister_menu_item('title','add');
